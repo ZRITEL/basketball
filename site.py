40d802972055551bd27f4ed26522c16280d57e3f
ZRITEL <ZRITEL@protonmail.ch>
2018-11-12 07:37:01 +0400

    Попытка реализовать чтение коэфицентов

--- site.py
+++ site.py
@@ -12,6 +12,23 @@ site_tabl = site + "standings;table;overall"
 url = requests.get(site_tabl)
 html_tabl = url.text
 
+#Блок загрузки страницы с коэфицентами
+site_kx = site + "odds-comparison;home-away;ft-including-ot"
+url = requests.get(site_kx)
+html_kx = url.text
+
+#Блок с поиском коэфицентов
+start = html.find("<td class=\"kx o_1\">")
+print(start)
+finish = html.find("class=\"odds-wrap\"", start) + 28
+print(finish)
+kx_1 = html[finish:finish + 4]
+start = html.find("<td class=\"kx o_2\">")
+print(start)
+finish = html.find("class=\"odds-wrap\"", start) + 28
+print(finish)
+kx_2 = html[finish:finish + 4]
+
 # Блок вывода загаловка страницы
 start = html.find("<title>") + 7   # Определяем позицию первого символа загаловка
 finish = html.find("</title>")     # Определяем позицию последнего символа заголовка
@@ -24,6 +41,7 @@ start = html.find("return false;\">", home) + 15
 finish = html.find("</a>", start)
 team_1 = html[start:finish]
 print("Команда 1: ", team_1)
+print("Коэфицент на П1: ", kx_1)
 
 #Блок вывода гостевой команды
 home = html.find("<div class=\"team-text tname-away\">")
@@ -31,4 +49,9 @@ start = html.find("return false;\">", home) + 15
 finish = html.find("</a>", start)
 team_2 = html[start:finish]
 print("Команда 2: ", team_2)
+print("Коэфицент на П2: ", kx_2)
 
+#Тест с выводом страницы в документ
+file = open("test.txt", "w")
+file.write(html_kx)
+file.close()
